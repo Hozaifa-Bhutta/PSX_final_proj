7b7d4bafde4c0b6fde1674a524852c9e428c11d3
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-21 11:32:41 -0500

    changed game and platform

--- game.py
+++ game.py
@@ -1,7 +1,16 @@
 import pygame
+import platform
 pygame.init()
 WIDTH = 1200
 HEIGHT = 1000
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-while True:
+done = False
+platforms = pygame.sprite.Group()
+while not done:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+	screen.fill((0,0,200))
+	platforms.add(platform((0,200,0),50,50))
+	presed = pygame.key.get_pressed()
 	pygame.display.flip()
--- platform.py
+++ platform.py
@@ -1,12 +1,12 @@
 import pygame
 
-lass platform(pygame.sprite.Sprite):
+class platform(pygame.sprite.Sprite):
 	def __init__(self, color, width, height):
 		# Call the parent class (Sprite) constructor
 		super().__init__()
-
+		
 		#Creates image
 		self.image = pygame.Surface([width,height])
 		# Makes Blue image
 		self.image.fill((0,200,0))
-        self.image.set_colorkey(WHITE)
+		self.image.set_colorkey(WHITE)
