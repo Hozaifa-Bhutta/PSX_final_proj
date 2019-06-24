1bdd2da0ec2734ddec85623af13c400fb7f4ee96
Daniel Song <djsong712@gmail.com>
2019-06-24 10:39:23 -0500

    h

--- make_player.py
+++ make_player.py
@@ -13,12 +13,16 @@ class player(pygame.sprite.Sprite):
 		self.y = y
 		self.screen = screen
 		self.rect = pygame.Rect(self.x,self.y,40,60)
+		self.speed = 1
 	def draw_player(self):
 		pygame.draw.rect(self.screen,self.color, self.rect)
 	
+	def gravity(self):
+		pass
+
 	def jump(self):
-		 for x in range(11):
-		 	self.rect.y -= 1
+		for x in range(1,11):
+			self.rect.y -= 1
 
 	def go_left(self):
 		self.rect.x += -6
