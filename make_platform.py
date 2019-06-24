b93f562abc1e5e3b391bcca5fc28438a60bde649
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-24 09:58:26 -0500

    fixed platform

--- make_platform.py
+++ make_platform.py
@@ -10,6 +10,12 @@ class platform(pygame.sprite.Sprite):
 		'''self.image = pygame.image.load("platform.png").convert_alpha()
 		self.rect = self.image.get_rect()
 		self.rect.center = screen.center'''
+		self.color = color
+		self.screen = screen
+		self.height = height
+		self.x = x
+		self.y = y
+		self.width = width
 		self.rect = pygame.Rect(x,y,width,height)
 	def draw_wall(self):	
-		pygame.draw.rect(screen,color,self.rect)
+		pygame.draw.rect(self.screen,self.color,self.rect)
