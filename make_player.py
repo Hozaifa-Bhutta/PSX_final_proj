2524accf742e58c44617101c8130b2cfdc7a845d
Daniel Song <djsong712@gmail.com>
2019-06-24 09:44:27 -0500

    he

--- make_player.py
+++ make_player.py
@@ -1,7 +1,14 @@
 import pygame
 
 class player(pygame.sprite.Sprite):
-	def __init__(self, color, width, height, x, y,screen):
+	def __init__(self):
 
 		super().__init__()
+		
+		RED = (255, 0, 0)
+
+		width = 40
+		height = 60
+		self.image = pygame.Surface([width, height])
+		self.image.fill(RED)
 		
