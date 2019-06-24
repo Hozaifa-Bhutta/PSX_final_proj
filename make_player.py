bd01c1a6ecfe48065f96d3ed46d49d7817899a35
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-24 09:49:11 -0500

    changed player

--- make_player.py
+++ make_player.py
@@ -1,14 +1,16 @@
 import pygame
 
 class player(pygame.sprite.Sprite):
-	def __init__(self):
+	def __init__(self,x,y):
 
 		super().__init__()
 		
 		RED = (255, 0, 0)
 
-		width = 40
-		height = 60
+		self.width = 40
+		self.height = 60
+		self.x = x
+		self.y = y
 		self.image = pygame.Surface([width, height])
 		self.image.fill(RED)
 	def draw_player(self):
