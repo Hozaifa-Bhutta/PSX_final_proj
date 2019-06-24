e2d73308db67bbfa06d9cf42a9ead7a6fe28534a
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-24 09:28:54 -0500

    changed game

--- game.py
+++ game.py
@@ -1,16 +1,18 @@
 import pygame
-import platform
+from make_platform import platform
 pygame.init()
 WIDTH = 1200
 HEIGHT = 1000
+BLUE = (0,0,220)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 done = False
 platforms = pygame.sprite.Group()
+platform1 = platform(BLUE, 20, 20, 30, 30, screen)
+platforms.add(platform1)
+ 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-	screen.fill((0,0,200))
-	platforms.add(platform((0,200,0),50,50))
 	presed = pygame.key.get_pressed()
 	pygame.display.flip()
