f20c16a0e9314dba87075b912a43356f886b6652
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-24 09:18:40 -0500

    changed make_platform

--- make_platform.py
+++ make_platform.py
@@ -1,12 +1,13 @@
 import pygame
 
 class platform(pygame.sprite.Sprite):
-	def __init__(self, color, width, height):
+	def __init__(self, color, width, height, x, yscreen):
 		# Call the parent class (Sprite) constructor
 		super().__init__()
 		
 		#Creates image
-		self.image = pygame.Surface([width,height])
 		# Makes Blue image
-		self.image.fill((0,200,0))
-		self.image.set_colorkey(WHITE)
+		'''self.image = pygame.image.load("platform.png").convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.center = screen.center'''
+		self.rect = pygame.rect(x,y,width,height)
