f5a09595c50e7e33a1a50952ad829354aa95b3c4
Hozaifa Bhutta <hozaifabhutta@gmail.com>
2019-06-26 09:41:55 -0500

    collision/right

--- game.py
+++ game.py
@@ -4,13 +4,15 @@ from make_player import player
 def is_colliding(platforms, player):
 	for platform in platforms:	
 		if player.rect.colliderect(platform.rect):
-			platform_list = range(platform.rect.y-10,platform.rect.y+10)
+			platform_list_y = range(platform.rect.y-10,platform.rect.y+10)
 			if (player.rect.y-30) ==(platform.rect.y-platform.rect.height):
 				return 'Up'
-			if (player.rect.y +50) in platform_list:
+			elif (player.rect.y +50) in platform_list_y:
 				return 'Down'
-			if player.rect.x + 10 == platform.rect.x:
+			elif (platform.rect.x)-(player.rect.x + 40) <=3: #and bool(set(range(player.rect.y-60,player.rect.y)) and set(range(platform.rect.y-platform.height,platform.rect.y))):
 				return 'Right'
+			else:
+				return 'Left'
 
 clock = pygame.time.Clock()
 
@@ -30,8 +32,9 @@ platform1 = platform(BLUE, 100, 20, 450, 400, screen)
 platforms.add(platform1)
 platforms.add(platform(GREEN, 200, 20 ,550, 300,screen))
 player1 = player(100,500,screen,platforms)
- 
+test = pygame.Rect(player1.x,player1.y,10,10)
 while not done:
+
 	previous_y = player1.rect.y
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -42,7 +45,8 @@ while not done:
 	if pressed[pygame.K_UP]:
 		for i in range(0,5):
 			if is_colliding(platforms,player1) == 'Up':
-				print ('Don\'t go up')
+				#print ('Don\'t go up')
+				pass
 			else:
 				player1.jump()
 
@@ -50,16 +54,20 @@ while not done:
 
 		player1.jumping == False
 	if pressed[pygame.K_RIGHT]:
-		if is_colliding(platforms,player1):
+		if is_colliding(platforms,player1) == 'Right':
 			print ('Don\'t go right')
-		player1.go_right()
+		else:
+			player1.go_right()
 
 	if pressed[pygame.K_LEFT]:
-		player1.go_left()
+		if is_colliding(platforms,player1) == 'Left':
+			print ('don\'t go left')
+		else:
+			player1.go_left()
 
 	
 	if is_colliding(platforms,player1) == 'Down':
-		print ('Dont go down')
+		#print ('Dont go down')
 		player1.velocity = 1
 	else:
 		player1.gravity()
@@ -69,7 +77,8 @@ while not done:
 		platform.draw()
 		platform.update()
 	player1.draw_player()
-	player1.rect.x-=1
+	pygame.draw.rect(screen, BLUE,test)
+	#player1.rect.x-=1
 
 
 	pygame.display.flip()
